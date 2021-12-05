0f715703e4163fe976b8ac8235513de9ce36c22a
Hamidreza Marzbani <hamied.reza.m@gmail.com>
2021-12-05 18:47:56 +0330

    player.dart ui is done

---
+++ lib/style.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+class Style  {
+
+  static const Color gray48 = Color(0xff484848);
+  static Color iconBack = const Color(0xff121212).withOpacity(0.30);
+}
--- lib/view/player.dart
+++ lib/view/player.dart
@@ -1,10 +1,12 @@
+import 'dart:math';
 import 'dart:ui';
+import 'package:castalk/style.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:blur/blur.dart';
 import 'package:flutter/widgets.dart';
 import 'package:flutter_svg/flutter_svg.dart';
-import 'package:step_progress_indicator/step_progress_indicator.dart';
+import 'package:music_slider/music_slider.dart';
 
 class Player extends StatefulWidget{
   const Player({Key? key}) : super(key: key);
@@ -18,6 +20,7 @@ class Player extends StatefulWidget{
 
 class PlayerState extends State<Player>
 {
+//  MusicSliderController _sliderController = MusicSliderController();
   String svgPath="assets/icons/";
   @override
   Widget build(BuildContext context) {
@@ -30,39 +33,92 @@ class PlayerState extends State<Player>
 
 
 
-      Blur(blur: 6.5,blurColor: Colors.transparent,child: Image.network("https://picsum.photos/1080/1920",width: w,height: h,fit: BoxFit.fill, colorBlendMode: BlendMode.overlay,)),
+      Blur(blur: 6.5,blurColor: Colors.lightBlueAccent.shade100,child: Image.network("https://picsum.photos/id/1032/1080/1920",width: w,height: h,fit: BoxFit.fill, colorBlendMode: BlendMode.overlay,)),
 
 
+
+      tripleImageCorner(w, h),
+      rightBottons(),
       Padding(
         padding: const EdgeInsets.only(top: 24,left: 24),
         child: Container(width: 44,height: 44,child: const Icon(Icons.arrow_back_outlined) ,decoration: BoxDecoration(color: Colors.white.withOpacity(0.4),shape: BoxShape.rectangle,borderRadius: BorderRadius.circular(12)),),
       ),
-      tripleImageCorner(w, h),
-      rightBottons(),
-      Column(children: [
-        Text("Podcast Name",style: Theme.of(context).textTheme.headline3,),
-        Text("Episode name which is long...",style: Theme.of(context).textTheme.headline1!.copyWith(fontSize: 22,fontWeight: FontWeight.w400),),
-        Row(
-          children: [
-            SvgPicture.asset(svgPath+"mic.svg"),
-            Padding(
-              padding: const EdgeInsets.only(left:5),
-              child: Text("Host Name",style: Theme.of(context).textTheme.bodyText2!.copyWith(color: const Color(0xff0E0E0E)),),
-            ),
-          ],
-        ),
+      Positioned(child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Text("Podcast Name",style: Theme.of(context).textTheme.headline3,),
+          Padding(
+            padding: const EdgeInsets.only(top: 15,bottom: 5),
+            child: Text("Episode name which is long...",style: Theme.of(context).textTheme.headline1!.copyWith(fontSize: 22,fontWeight: FontWeight.w400),),
+          ),
+          Row(
+            children: [
+              SvgPicture.asset(svgPath+"mic.svg",),
+              Padding(
+                padding: const EdgeInsets.only(left:5),
+                child: Text("Host Name",style: Theme.of(context).textTheme.bodyText2!.copyWith(color: const Color(0xff0E0E0E)),),
+              ),
+            ],
+          ),
+          Padding(
+            padding: const EdgeInsets.only(top: 32),
+            child: SizedBox(width: w,height: 80,child: MusicSlider(
+              emptyColors: const [Colors.white],
+              fillColors: const [
+                Color(0xff484848),
+              ],
+              controller: MusicSliderController(initialValue: 0.5),
+              animateWaveByTime: false,
+              height: 50,
+              division: 53,
 
-        const StepProgressIndicator(
-          totalSteps: 15,
-          currentStep: 12,
-          size: 40,
-          selectedColor: Color(0xffFF5959),
-          unselectedColor: Colors.white,
-          roundedEdges: Radius.circular(50),
+              wave: (x, t, a) => a * cos(x * 0.32) * sin(x * 0.23),
+            )),
+          ),
+          Align(child: RichText(text: const TextSpan(children:<TextSpan>[
+            TextSpan(text: '12 : 31',style: TextStyle(color: Style.gray48,fontWeight: FontWeight.w500,fontSize: 14)),
+            TextSpan(text: ' / ', style:  TextStyle(color: Colors.white,fontWeight: FontWeight.w400,fontSize: 14)),
+            TextSpan(text: '26 : 45',style:  TextStyle(color: Colors.white,fontWeight: FontWeight.w400,fontSize: 14)),
 
-        ),
+          ]),),alignment: Alignment.center,),
+          Padding(
+            padding: const EdgeInsets.only(top: 23),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+              InkWell(child: SvgPicture.asset(svgPath+"pre.svg"),),
+              Container(padding: const EdgeInsets.all(14),width: 47,height: 47,child: SvgPicture.asset(svgPath+"circle_left.svg") ,decoration: BoxDecoration(color: Style.iconBack,shape: BoxShape.rectangle,borderRadius: BorderRadius.circular(12)),),
+              Container(padding: const EdgeInsets.all(26),width: 80,height: 80,child: SvgPicture.asset(svgPath+"pause.svg",width: 28,height: 28,) ,decoration: BoxDecoration(color: Colors.white.withOpacity(0.49),shape: BoxShape.rectangle,borderRadius: BorderRadius.circular(28)),),
+              Container(padding: const EdgeInsets.all(14),width: 47,height: 47,child: SvgPicture.asset(svgPath+"circle_righ.svg") ,decoration: BoxDecoration(color: Style.iconBack,shape: BoxShape.rectangle,borderRadius: BorderRadius.circular(12)),),
+              InkWell(child: SvgPicture.asset(svgPath+"forw.svg"),),
 
-      ],)
+            ],),
+          ),
+
+          Padding(
+            padding: const EdgeInsets.only(top: 23),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Container(padding: const EdgeInsets.all(14),width: 47,height: 47,child: SvgPicture.asset(svgPath+"setting.svg") ,decoration: BoxDecoration(color: Style.iconBack,shape: BoxShape.rectangle,borderRadius: BorderRadius.circular(12)),),
+                Container(padding: const EdgeInsets.all(0),width: 102,height: 47,child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    SvgPicture.asset(svgPath+"comment.svg",width: 19,height: 19,),
+                    const Text('254',style:  TextStyle(color: Colors.white,fontWeight: FontWeight.w400,fontSize: 14)),
+
+                  ],
+                ) ,decoration: BoxDecoration(color: Colors.white.withOpacity(0.49),shape: BoxShape.rectangle,borderRadius: BorderRadius.circular(12)),),
+                Container(padding: const EdgeInsets.all(14),width: 47,height: 47,child: SvgPicture.asset(svgPath+"playlist.svg") ,decoration: BoxDecoration(color: Style.iconBack,shape: BoxShape.rectangle,borderRadius: BorderRadius.circular(12)),),
+
+              ],),
+          )
+
+
+        ],
+      ),top: h/1.9,left: 42,right: 42,),
+
+
 
     ],)
     );
@@ -110,27 +166,61 @@ class PlayerState extends State<Player>
 
   rightBottons()
   {
-    Color iconBack=Color(0xff121212).withOpacity(0.30);
    return Positioned(right: 30,top: 50,child: Column(
      children: [
        Padding(
          padding: const EdgeInsets.only(top: 39,left: 0),
-         child: Container(padding: EdgeInsets.all(14),width: 50,height: 50,child: SvgPicture.asset(svgPath+"share.svg") ,decoration: BoxDecoration(color: iconBack,shape: BoxShape.rectangle,borderRadius: BorderRadius.circular(12)),),
+         child: Container(padding: EdgeInsets.all(14),width: 50,height: 50,child: SvgPicture.asset(svgPath+"share.svg") ,decoration: BoxDecoration(color: Style.iconBack,shape: BoxShape.rectangle,borderRadius: BorderRadius.circular(12)),),
        ),
        Padding(
          padding: const EdgeInsets.only(top: 39,left: 0),
-         child: Container(padding: EdgeInsets.all(14),width: 50,height: 50,child: SvgPicture.asset(svgPath+"addlist.svg") ,decoration: BoxDecoration(color: iconBack,shape: BoxShape.rectangle,borderRadius: BorderRadius.circular(12)),),
+         child: Container(padding: EdgeInsets.all(14),width: 50,height: 50,child: SvgPicture.asset(svgPath+"addlist.svg") ,decoration: BoxDecoration(color: Style.iconBack,shape: BoxShape.rectangle,borderRadius: BorderRadius.circular(12)),),
        ),
        Padding(
          padding: const EdgeInsets.only(top: 39,left: 0),
-         child: Container(padding: EdgeInsets.all(14),width: 50,height: 50,child: SvgPicture.asset(svgPath+"like.svg") ,decoration: BoxDecoration(color: iconBack,shape: BoxShape.rectangle,borderRadius: BorderRadius.circular(12)),),
+         child: Container(padding: EdgeInsets.all(14),width: 50,height: 50,child: SvgPicture.asset(svgPath+"like.svg") ,decoration: BoxDecoration(color: Style.iconBack,shape: BoxShape.rectangle,borderRadius: BorderRadius.circular(12)),),
        ),
        Padding(
          padding: const EdgeInsets.only(top: 39,left: 0),
-         child: Container(padding: EdgeInsets.all(14),width: 50,height: 50,child: SvgPicture.asset(svgPath+"download.svg") ,decoration: BoxDecoration(color: iconBack,shape: BoxShape.rectangle,borderRadius: BorderRadius.circular(12)),),
+         child: Container(padding: EdgeInsets.all(14),width: 50,height: 50,child: SvgPicture.asset(svgPath+"download.svg") ,decoration: BoxDecoration(color: Style.iconBack,shape: BoxShape.rectangle,borderRadius: BorderRadius.circular(12)),),
        ),
      ],));
   }
 
+  playerSection()
+  {
+      return  Positioned.fill(child: Align(child: Column(
+          children: [
+            Text("Podcast Name",style: Theme.of(context).textTheme.headline3,),
+            Text("Episode name which is long...",style: Theme.of(context).textTheme.headline1!.copyWith(fontSize: 22,fontWeight: FontWeight.w400),),
+            Row(
+              children: [
+                SvgPicture.asset(svgPath+"mic.svg"),
+                Padding(
+                  padding: const EdgeInsets.only(left:5),
+                  child: Text("Host Name",style: Theme.of(context).textTheme.bodyText2!.copyWith(color: const Color(0xff0E0E0E)),),
+                ),
+              ],
+            ),
+
+            Padding(
+              padding: const EdgeInsets.all(10),
+              child: MusicSlider(
+                emptyColors: const [Colors.white],
+                fillColors: const [
+                  Color(0xff484848),
+                ],
+                controller: MusicSliderController(initialValue: 0.5),
+                animateWaveByTime: false,
+                height: 50,
+                division: 53,
+
+                wave: (x, t, a) => a * cos(x * 0.32) * sin(x * 0.23),
+              ),
+            ),
+
+          ]),alignment: Alignment.bottomCenter,));
+
+  }
 
 }
