e3d693f3f726661a4dd05f693a48a36fc61c4e8c
Hamidreza Marzbani <hamied.reza.m@gmail.com>
2021-12-15 11:24:09 +0330

    other_user_profile.dart ui is complete and bottom list and search added also header scroll fixed

--- lib/view/other_user_profile.dart
+++ lib/view/other_user_profile.dart
@@ -23,6 +23,7 @@ class OtherUserProfile extends StatefulWidget{
 class OtherUserProfileState extends State<OtherUserProfile>{
 
   String svgPath="assets/icons/";
+  TextEditingController numberController = TextEditingController();
 
 
   @override
@@ -30,10 +31,11 @@ class OtherUserProfileState extends State<OtherUserProfile>{
 
 
     double w = MediaQuery.of(context).size.width;
+    double h = MediaQuery.of(context).size.height;
 
     return Scaffold(
         backgroundColor: Theme.of(context).backgroundColor,
-        appBar: PreferredSize(preferredSize: Size(w,300),
+        appBar: PreferredSize(preferredSize: Size(w,200),
             child:Stack(children: [
               Container(
                   width: w,
@@ -62,14 +64,13 @@ class OtherUserProfileState extends State<OtherUserProfile>{
                   ))),
 
               Padding(
-                padding: const EdgeInsets.only(left: 24,top: 39),
-                child: Align(alignment: Alignment.centerLeft,
+                padding: const EdgeInsets.only(left: 24),
+                child: Align(alignment: Alignment.bottomLeft,
                     child: Row(children: [
                       ClipRRect(child: Image.network("https://picsum.photos/96"),borderRadius: const BorderRadius.all(Radius.circular(20)),),
                       Padding(
                         padding: const EdgeInsets.only(left: 10,top: 40),
                         child: Row(
-
                           children: [
                             Text("Host Name",style: Theme.of(context).textTheme.headline1),
                             Padding(
@@ -83,49 +84,47 @@ class OtherUserProfileState extends State<OtherUserProfile>{
                     ],)),
               ),
 
-              Padding(
-                padding: const EdgeInsets.only(left: 62,bottom: 22,),
-                child: Align(
-                    alignment: Alignment.bottomLeft,
-                    child: Column(
-                      mainAxisAlignment: MainAxisAlignment.end,
-                      children: [
-                        Text("Listens",style: Theme.of(context).textTheme.subtitle1!.copyWith(fontWeight: FontWeight.w500),),
-                        Text("12K",style: Theme.of(context).textTheme.headline1),
-                      ],)),
-              ),
-              Padding(
-                padding: const EdgeInsets.only(left: 0,bottom: 22),
-                child: Align(
-                    alignment: Alignment.bottomCenter,
-                    child: Column(
-                      mainAxisAlignment: MainAxisAlignment.end,
-                      children: [
-                        Text("Followers",style: Theme.of(context).textTheme.subtitle1!.copyWith(fontWeight: FontWeight.w500),),
-                        Text("12K",style: Theme.of(context).textTheme.headline1),
-                      ],)),
-              ),
-              Padding(
-                padding: const EdgeInsets.only(right: 62,bottom: 22),
-                child: Align(
-                    alignment: Alignment.bottomRight,
-                    child: Column(
-                      mainAxisAlignment: MainAxisAlignment.end,
-                      children: [
-                        Text("Posts",style: Theme.of(context).textTheme.subtitle1!.copyWith(fontWeight: FontWeight.w500),),
-                        Text("12K",style: Theme.of(context).textTheme.headline1),
-                      ],)),
-              )
+
+
+
 
 
 
             ],)),
           body: SingleChildScrollView(
             child: Column(
+              mainAxisSize: MainAxisSize.min,
               crossAxisAlignment: CrossAxisAlignment.start,
               children: [
+
                 Padding(
-                  padding: const EdgeInsets.only(left: 27,bottom: 9),
+                  padding: const EdgeInsets.only(top: 20),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: [
+                    Column(
+                      mainAxisAlignment: MainAxisAlignment.end,
+                      children: [
+                        Text("Listens",style: Theme.of(context).textTheme.subtitle1!.copyWith(fontWeight: FontWeight.w500),),
+                        Text("12K",style: Theme.of(context).textTheme.headline1),
+                      ],),
+                    Column(
+                      mainAxisAlignment: MainAxisAlignment.end,
+                      children: [
+                        Text("Followers",style: Theme.of(context).textTheme.subtitle1!.copyWith(fontWeight: FontWeight.w500),),
+                        Text("12K",style: Theme.of(context).textTheme.headline1),
+                      ],),
+                    Column(
+                      mainAxisAlignment: MainAxisAlignment.end,
+                      children: [
+                        Text("Posts",style: Theme.of(context).textTheme.subtitle1!.copyWith(fontWeight: FontWeight.w500),),
+                        Text("12K",style: Theme.of(context).textTheme.headline1),
+                      ],)
+
+                  ],),
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(left: 27,bottom: 9,top: 36),
                   child: Text("About:",style: Style.t_500_14w,),
                 ),
                 Padding(
@@ -141,36 +140,295 @@ class OtherUserProfileState extends State<OtherUserProfile>{
                     moreStyle: Style.t_400_12g.copyWith(fontStyle: FontStyle.italic),
                   ),
                 ),
-                ElevatedButton(onPressed:() => print(""), child: Row(
-                  crossAxisAlignment: CrossAxisAlignment.center,
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    SvgPicture.asset(svgPath+"coin.svg"),
-                    Padding(
-                      padding: const EdgeInsets.only(left: 8),
-                      child: Text("Buy me a Coffee",style: Style.t_500_18w,),
-                    ),
-                  ],
-                ) ,style: ButtonStyle(
-                    fixedSize: MaterialStateProperty.all(Size(w,58)),
-                    shape: MaterialStateProperty.all<RoundedRectangleBorder>(
-                        RoundedRectangleBorder(
-                            borderRadius: BorderRadius.circular(16.0),
-                            side: const BorderSide(color: Style.gray90)
-                        )
-                    ),
-                    backgroundColor:MaterialStateProperty.all(Style.background)),),
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 25),
+                  child: ElevatedButton(onPressed:() => print(""), child: Row(
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      SvgPicture.asset(svgPath+"coin.svg"),
+                      Padding(
+                        padding: const EdgeInsets.only(left: 8),
+                        child: Text("Buy me a Coffee",style: Style.t_500_18w,),
+                      ),
+                    ],
+                  ) ,style: ButtonStyle(
+                      fixedSize: MaterialStateProperty.all(Size(w,58)),
+                      shape: MaterialStateProperty.all<RoundedRectangleBorder>(
+                          RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(16.0),
+                              side: const BorderSide(color: Style.gray90)
+                          )
+                      ),
+                      backgroundColor:MaterialStateProperty.all(Style.background)),),
+                ),
 
                 _playerSection(),
                 _checkoutSection(),
-                _bottomButtonSection()
+                _bottomButtonSection(),
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.only(bottom: 13,top: 35,left: 26),
+                      child: Text("Latest Podcasts:",style: Style.t_500_14g,),
+                    ),
+                    _LatestPodcastSection(w),
 
+                  ],),
+
+                Padding(
+                  padding: const EdgeInsets.only(top: 0,left: 27),
+                  child: Text("All Episodes:",style: Style.t_500_14w),
+                ),
+                _searchBar(w),
+                Flexible(child: ListView(
+                  physics: const NeverScrollableScrollPhysics(),
+                  shrinkWrap: true,
+                  children: [
+                    _likedItem(w,h),
+                    _likedItem(w,h),
+                    _likedItem(w,h),
+                  ],
+                ))
               ],
             ),
           ),
 
     );
   }
+
+  _likedItem(w,h)
+  {
+    return Padding(
+      padding: const EdgeInsets.only(top: 8,left: 20,right: 20),
+      child: Column(
+        children: [
+          Row(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Stack(
+                children: [
+                  Container(
+                    padding: const EdgeInsets.all(25),
+
+                    height: 96,
+                    width: 96,
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(12),
+                      image: const DecorationImage(
+                        image: NetworkImage('https://picsum.photos/96/96'),
+                        fit: BoxFit.cover,
+                      ),
+                    ),
+                    child: Container(
+                      width: 77,
+                      height: 77,
+                      padding: const EdgeInsets.all(15),
+                      child: SvgPicture.asset(
+                        svgPath + "play.svg",
+                      ),
+                      decoration: BoxDecoration(
+                          color: Style.gray32,
+                          shape: BoxShape.rectangle,
+                          borderRadius: BorderRadius.circular(12)),
+                    ) /* add child content here */,
+                  ),
+                ],
+              ),
+              Expanded(child: Padding(
+                padding: const EdgeInsets.only(left: 14),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      "Episode name which is long...".length > 30
+                          ? "Episode name which is long...".substring(0, 30) + "..."
+                          : "Episode name which is long...",
+                      style: Style.t_500_14w,
+                    ),
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Padding(
+                          padding: const EdgeInsets.only(top: 15),
+                          child: Row(
+                            children: [
+                              SvgPicture.asset(svgPath+"timer.svg"),
+                              Padding(
+                                padding: const EdgeInsets.only(left: 5),
+                                child: Text("1 : 26 : 45",
+                                    style: Style.t_400_12_grayA1),
+                              ),
+                            ],
+                          ),
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.only(top: 12),
+                          child: Row(
+                            children: [
+                              SvgPicture.asset(svgPath+"heart_empty.svg"),
+                              Padding(
+                                padding: const EdgeInsets.only(left: 5),
+                                child: Text("250",
+                                    style:  Style.t_400_12_grayA1),
+                              ),
+                            ],
+                          ),
+                        )
+                      ],
+                    ),
+                  ],
+                ),
+              )),
+              Container(
+                height: 96,
+                width: 44,
+
+                padding: const EdgeInsets.all(18),
+                decoration: BoxDecoration(color: Style.gray48op50,borderRadius: BorderRadius.circular(12)),
+                child: SvgPicture.asset(
+                    svgPath + "arrow_left.svg",
+                    width: 12,
+                    height: 6),
+              )
+            ],
+          ),
+          const Padding(
+            padding: EdgeInsets.only(bottom: 8,right: 12,left: 12,top: 8),
+            child: Divider(height: 1,color: Style.divider,thickness: 1,),
+          )
+        ],
+      ),
+    );
+
+  }
+
+
+  _searchBar(w)
+  {
+    return Padding(
+      padding: const EdgeInsets.only(top: 20,right: 14,left: 14,bottom: 30),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceAround,
+        children: [
+          Container(
+              height: 55,
+              decoration: BoxDecoration(
+                  borderRadius:
+                  const BorderRadius.all(Radius.circular(12)),
+                  border: Border.all(
+                      width: 1, color: const Color(0xff484848))),
+              width: w / 2,
+              child: TextField(
+                  controller: numberController,
+                  textAlign: TextAlign.left,
+                  maxLines: 1,
+                  decoration: InputDecoration(
+                      border: InputBorder.none,
+                      isDense: false,
+                      contentPadding: const EdgeInsets.only(
+                          top: 12, bottom: 12, left: 19),
+                      hintText: "Type to Search...",
+                      hintStyle:
+                      TextStyle(color: Theme.of(context).hintColor),
+                      fillColor: Colors.white))),
+          Container(
+            padding: const EdgeInsets.all(12),
+
+            width: 44,
+            height: 44,
+            child: SvgPicture.asset(svgPath+"search.svg",width: 14,height: 14,),
+            decoration: BoxDecoration(
+                color: Style.headerBackBtn,
+                shape: BoxShape.rectangle,
+                borderRadius: BorderRadius.circular(12)),
+          ),
+          Container(
+            width: 44,
+            height: 44,
+            padding: const EdgeInsets.all(12),
+            child: SvgPicture.asset(svgPath+"filter.svg",width: 14,height: 14,),
+            decoration: BoxDecoration(
+                color: Style.glassBlack,
+                shape: BoxShape.rectangle,
+                borderRadius: BorderRadius.circular(12)),
+          ),
+          Container(
+            padding: const EdgeInsets.all(12),
+            width: 44,
+            height: 44,
+            child: SvgPicture.asset(svgPath+"sort.svg",width: 14,height: 14,),
+            decoration: BoxDecoration(
+                color: Style.glassBlack,
+                shape: BoxShape.rectangle,
+                borderRadius: BorderRadius.circular(12)),
+          )
+        ],
+      ),
+    );
+
+  }
+
+  _LatestPodcastSection(w)
+  {
+    return SizedBox(
+      width: w,
+      height: 220,
+      child:
+
+      ListView.builder(
+        itemBuilder: (context, index) {
+          return  _podcastItem();
+
+        },
+        scrollDirection: Axis.horizontal,
+      ),
+
+    )
+    ;
+  }
+
+  _podcastItem(){
+    return  Padding(
+      padding: const EdgeInsets.only(right: 16),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Container(
+            height: 122,
+            width: 122,
+            decoration: BoxDecoration(
+              borderRadius: BorderRadius.circular(20),
+              image: const DecorationImage(
+                image: NetworkImage('https://picsum.photos/122/122'),
+                fit: BoxFit.cover,
+              ),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.only(top: 10),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text("Episode Name and...",style: Style.t_500_14w,),
+                Padding(
+                  padding: const EdgeInsets.only(top: 4),
+                  child: Text("Artist and the others",style: Style.t_400_12_gray,),
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+
 
   _playerSection()
   {
