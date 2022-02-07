9c86da7ebb635a812fb85960649536c6ef63ab93
Marzbani <hamied.reza.m@gmail.com>
2022-02-07 16:47:55 +0330

    file_api.dart requests added

---
+++ lib/apis/file_api.dart
@@ -0,0 +1,88 @@
+import 'dart:convert';
+
+import 'package:castalk/apis/base_api.dart';
+import 'package:http/http.dart' as http;
+
+class FileApi{
+
+
+  uploadFile(
+      {required var token,required var path}) async
+  {
+
+    var headers = {
+      'Authorization': 'Bearer $token',
+    };
+
+    var request = http.MultipartRequest('POST', Uri.parse(BaseApi.baseAddressSlash+'upload'));
+    request.fields.addAll({
+      'project': 'castalk'
+    });
+    request.files.add(await http.MultipartFile.fromPath('file', path));
+    request.headers.addAll(headers);
+
+    http.StreamedResponse response = await request.send();
+
+    if (response.statusCode == 200) {
+      print(await response.stream.bytesToString());
+    }
+    else {
+      print(response.reasonPhrase);
+    }
+  }
+
+  retriveFile(
+      {required var token,required var id}) async
+  {
+
+    var headers = {
+      'Authorization': 'Bearer $token',
+    };
+
+    var request = http.Request('GET', Uri.parse(BaseApi.baseAddressSlash+'find'));
+
+    request.body = json.encode({
+      "file_id": id
+    });
+    request.headers.addAll(headers);
+
+    http.StreamedResponse response = await request.send();
+
+    if (response.statusCode == 200) {
+      print(await response.stream.bytesToString());
+    }
+    else {
+      print(response.reasonPhrase);
+    }
+
+  }
+
+  uploadImage(
+      {required var token,required var file}) async
+  {
+
+    var headers = {
+      'Authorization': 'Bearer $token',
+    };
+
+    var request = http.MultipartRequest('POST', Uri.parse(BaseApi.baseAddressSlash+'upload'));
+    request.files.add(await http.MultipartFile.fromPath('image', file));
+    request.headers.addAll(headers);
+
+    http.StreamedResponse response = await request.send();
+
+    if (response.statusCode == 200) {
+      print(await response.stream.bytesToString());
+    }
+    else {
+      print(response.reasonPhrase);
+    }
+
+
+  }
+
+
+
+
+
+}
